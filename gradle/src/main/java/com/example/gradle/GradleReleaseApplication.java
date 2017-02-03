e4ad2788332a2ccefc330d8896956a7f0891ceba
wernerr <rafael.werner@ericsson.com>
2017-02-03 16:39:31 +0100

    correct initial version

---
+++ gradle/src/main/java/com/example/gradle/GradleReleaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleReleaseApplication.class, args);
+	}
+}
