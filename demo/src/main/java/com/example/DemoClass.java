fbca2b708cccd66c771f8b5ec49eed9dfa8783ab
wernerr <rafael.werner@ericsson.com>
2017-02-03 16:37:14 +0100

    initial version

---
+++ demo/src/main/java/com/example/DemoClass.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class DemoClass
+{
+   String myString;
+   int inteiro;
+   
+   public DemoClass(String s)
+   {
+       myString = s;
+   }
+   
+   public String getMyString()
+   {
+       return myString;
+   }
+   
+   public int getInteiro()
+   {
+       return inteiro;
+   }
+}
